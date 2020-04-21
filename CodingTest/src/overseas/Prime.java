ee3649d70371e0f1c39e0f7973a07a1a18336a0a
yeonsu100 <yeonsu100@gmail.com>
2020-04-21 12:20:46 +0900

    Prime

---
+++ CodingTest/src/overseas/Prime.java
@@ -0,0 +1,26 @@
+package overseas;
+
+import java.util.Scanner;
+
+public class Prime {
+	public static void main(String[] args) {
+		int temp, num;
+		boolean isPrime=true;
+		Scanner in=new Scanner(System.in);
+		num=in.nextInt();
+		in.close();
+		
+		for(int i=2; i<=num/2; i++) {
+			temp=num%i;
+			if(temp==0) {
+				isPrime=false;
+				break;
+			}
+		}
+		if(isPrime) {
+			System.out.println(num+" number is prime");
+		}else {
+			System.out.println(num+" number is not a prime");
+		}
+	}
+}
