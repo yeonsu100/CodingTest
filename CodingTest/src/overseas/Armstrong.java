434e142dbc3bacb5dc901aae5ca7138c6725395c
yeonsu100 <yeonsu100@gmail.com>
2020-04-24 20:08:59 +0900

    Armstrong number

---
+++ CodingTest/src/overseas/Armstrong.java
@@ -0,0 +1,21 @@
+package overseas;
+
+public class Armstrong {
+	public static void main(String[] args) {
+		int c=0, a, temp;
+		int n=153;
+		temp=n;
+		
+		while(n>0) {
+			a=n%10;
+			n=n/10;
+			c=c+(a*a*a);
+		}
+		
+		if(temp==c) {
+			System.out.println("Armstrong number");
+		}else {
+			System.out.println("Not armstrong number");
+		}
+	}
+}
