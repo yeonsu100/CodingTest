3be9a36fe14d419539db408380887c350337ae9e
yeonsu100 <yeonsu100@gmail.com>
2020-04-25 18:50:18 +0900

    Lucus Numbers - else if문 추가

---
+++ CodingTest/src/overseas/LucusNumbers.java
@@ -0,0 +1,24 @@
+package overseas;
+
+public class LucusNumbers {
+	public static void main(String[] args) {
+		System.out.print("\nFirst 10 Lucus numbers : ");
+		
+		int n=10;
+		int n1=2, n2=1, n3;
+		
+		if(n>1) {
+			System.out.println("\n2\n1");
+			for(int i=2; i<n; ++i) {
+				n3=n2;
+				n2+=n1;
+				n1=n3;
+				System.out.println(n2);
+			}
+		}else if(n==1) {
+			System.out.println("\n2");
+		}else {
+			System.out.println("Input a positive number, please!");
+		}
+	}
+}
