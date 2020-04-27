d1f521bd415dcb3fc6696578ba28c1723c168e6b
yeonsu100 <yeonsu100@gmail.com>
2020-04-27 22:21:38 +0900

    Check whether a given number is a happy number or not

---
+++ CodingTest/src/overseas/CheckHappyOrNot.java
@@ -0,0 +1,27 @@
+package overseas;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class CheckHappyOrNot {
+	public static boolean isHappyNum(int num) {
+		Set<Integer> unique_num=new HashSet<Integer>();
+		
+		while(unique_num.add(num)) {
+			int value=0;
+			while(num>0) {
+				value+=Math.pow(num%10, 2);
+				num/=10;
+			}
+			num=value;
+		}
+		return num==1;
+	}
+	
+	public static void main(String[] args) {
+		System.out.print("Input a number : ");
+		int num=new Scanner(System.in).nextInt();
+		System.out.println(isHappyNum(num));
+	}
+}
