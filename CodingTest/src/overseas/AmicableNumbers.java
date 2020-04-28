a5b779fd89940341d7c77a6ab738afa8e6311558
yeonsu100 <yeonsu100@gmail.com>
2020-04-28 22:04:43 +0900

    Armicable numbers

---
+++ CodingTest/src/overseas/AmicableNumbers.java
@@ -0,0 +1,34 @@
+package overseas;
+
+import java.util.Scanner;
+
+public class AmicableNumbers {
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		System.out.print("Input the first number : ");
+		int num1=in.nextInt();
+		System.out.print("Input the second number : ");
+		int num2=in.nextInt();
+		
+		int sum_num1=0, sum_num2=0;
+		
+		for(int i=1; i<=num1; i++) {
+			if(num1%i==0) {
+				sum_num1 += i;
+			}
+		}
+		
+		for(int i=1; i<=num2; i++) {
+			if(num2%i==0) {
+				sum_num2 += i;
+			}
+		}
+		
+		if(sum_num1==sum_num2) {
+			System.out.println("These numbers are amicable.");
+		}else {
+			System.out.println("These numbers are NOT amicable.");
+			System.out.println("\n");
+		}
+	}
+}
