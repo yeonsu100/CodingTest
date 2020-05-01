11536c2f2d3f97fdd8f8e6cb90df7114c08d8d6c
yeonsu100 <yeonsu100@gmail.com>
2020-05-01 21:05:44 +0900

    Make right-angle triangle

---
+++ CodingTest/src/overseas/RightAngleTriangle.java
@@ -0,0 +1,19 @@
+package overseas;
+
+import java.util.Scanner;
+
+public class RightAngleTriangle {
+	public static void main(String[] args) {
+		int i, j, n;
+		System.out.print("Input number of rows : ");
+		Scanner in=new Scanner(System.in);
+		n=in.nextInt();
+		
+		for(i=1; i<=n; i++) {
+			for(j=1; j<=i; j++) {
+				System.out.print(j);
+			}
+			System.out.println(" ");
+		}
+	}
+}
