0cabc16cd7afbf844cf0a87d4f8b503f4e60e55f
yeonsu100 <yeonsu100@gmail.com>
2020-05-02 21:21:44 +0900

    Prove that Euclid's Algorithm

---
+++ CodingTest/src/overseas/ProveThatEuclidsAlgorithm.java
@@ -0,0 +1,24 @@
+package overseas;
+
+public class ProveThatEuclidsAlgorithm {
+	public static int euclid(int x, int y) {
+		if(x==0||y==0) {
+			return 1;
+		}
+		if(x<y) {
+			int t=x;
+			x=y;
+			y=t;
+		}
+		if(x%y==0) {
+			return y;
+		}else {
+			return euclid(y, x%y);
+		}
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("result: " + euclid(48, 24));
+		System.out.println("result: " + euclid(125463, 9658));
+	}
+}
