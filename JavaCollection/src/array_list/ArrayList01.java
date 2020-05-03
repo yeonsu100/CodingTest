dea47ee03741b6ba38bbf7fc4fb28e83642d62f1
yeonsu100 <yeonsu100@gmail.com>
2020-05-03 20:30:47 +0900

    Array List - 01

---
+++ JavaCollection/src/array_list/ArrayList01.java
@@ -0,0 +1,15 @@
+package array_list;
+import java.util.*;
+
+public class ArrayList01 {
+	public static void main(String[] args) {
+		List<String> list_Strings=new ArrayList<String>();
+		list_Strings.add("Red");
+		list_Strings.add("Blue");
+		list_Strings.add("Black");
+		list_Strings.add("White");
+		list_Strings.add("Purple");
+		list_Strings.add("Orange");
+		System.out.println(list_Strings);
+	}
+}
