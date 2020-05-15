a2e1021f0543042d5baf2a6a682acca98ba8781e
yeonsu100 <yeonsu100@gmail.com>
2020-05-15 22:15:22 +0900

    TreeSet - 15

---
+++ JavaCollection/src/treeset/TreeSet16.java
@@ -0,0 +1,28 @@
+package treeset;
+
+import java.util.TreeSet;
+
+public class TreeSet16 {
+	public static void main(String[] args) {
+		// Creating TreeSet
+		TreeSet<Integer> tree_num=new TreeSet<Integer>();
+		TreeSet<Integer> tree_headset=new TreeSet<Integer>();
+		
+		// Add numbers in the tree
+		tree_num.add(10);
+		tree_num.add(22);
+		tree_num.add(36);
+		tree_num.add(43);
+		tree_num.add(55);
+		tree_num.add(61);
+		tree_num.add(75);
+		tree_num.add(84);
+		tree_num.add(97);
+		
+		System.out.println("Original tree set : "+tree_num);
+		System.out.println
+			("Removes 55 from the list : "+tree_num.remove(55));
+		System.out.println
+			("Tree set after removing the element : "+tree_num);
+	}
+}
