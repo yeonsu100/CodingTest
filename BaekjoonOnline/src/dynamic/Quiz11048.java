ee0811c2b1b07cc0b6384b1679e47f8242318f2e
yeonsu100 <yeonsu100@gmail.com>
2020-05-16 23:16:07 +0900

    Quiz11048 - 초기 셋팅

---
+++ BaekjoonOnline/src/dynamic/Quiz11048.java
@@ -0,0 +1,18 @@
+package dynamic;
+import java.util.*;
+
+public class Quiz11048 {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+		int n=Integer.parseInt(sc.next());
+		int m=Integer.parseInt(sc.next());
+		int[][] arr=new int[n+1][m+1];
+		for(int i=1; i<=n; i++) {
+			for(int j=1; j<=m; j++) {
+				
+			}
+		}
+		int[][] start=new int[n+1][m+1];
+	}
+}
