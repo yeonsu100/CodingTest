967a285cd54ebe5944248a40f9430acbca8dbe74
yeonsu100 <yeonsu100@gmail.com>
2020-05-24 21:57:44 +0900

    add a 'for statement'

--- HackerRank/src/hackerrank/CommonChild.java
+++ HackerRank/src/hackerrank/CommonChild.java
@@ -8,9 +8,16 @@ public class CommonChild {
 		String str2=sc.next();
 		
 		int result=commonChild(str1, str2);
+		
+		System.out.println(result);
 	}
 	
 	public static int commonChild(String str1, String str2) {
-		
+		int[][] valueLength=new int[str1.length()+1][str2.length()+1];
+		for(int i=0; i<str1.length(); i++) {
+			for(int j=0; j<str2.length(); j++) {
+				
+			}
+		}
 	}
 }
