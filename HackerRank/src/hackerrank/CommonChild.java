18e53308618c53c155986835a647e0dc76f56cb7
yeonsu100 <yeonsu100@gmail.com>
2020-05-24 21:46:07 +0900

    main method

---
+++ HackerRank/src/hackerrank/CommonChild.java
@@ -0,0 +1,11 @@
+package hackerrank;
+import java.util.*;
+
+public class CommonChild {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String str1=sc.next();
+		String str2=sc.next();
+		
+	}
+}
