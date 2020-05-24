6a5e7e088aec8bea9ca65d1067b8104537667070
yeonsu100 <yeonsu100@gmail.com>
2020-05-24 23:14:14 +0900

    TreeMap - 04 (create a tree map and print)

---
+++ JavaCollection/src/treemap/TreeMap04.java
@@ -0,0 +1,14 @@
+package treemap;
+import java.util.*;
+
+public class TreeMap04 {
+	public static void main(String[] args) {
+		TreeMap<String, String> tm=new TreeMap<String, String>();
+		tm.put("Frohana 1", "Elsa");
+		tm.put("Frohana 2", "Anna");
+		tm.put("Frohana 3", "Olaf");
+		tm.put("Frohana 4", "Jorgenbjorgen");
+		
+		System.out.println(tm);
+	}
+}
