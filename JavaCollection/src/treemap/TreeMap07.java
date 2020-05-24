1161da0b4c0aef052adbbf3c2ed483ff5d33a88f
yeonsu100 <yeonsu100@gmail.com>
2020-05-24 23:33:40 +0900

    TreeMap - 07 (리턴값 수정)

--- JavaCollection/src/treemap/TreeMap07.java
+++ JavaCollection/src/treemap/TreeMap07.java
@@ -17,6 +17,6 @@ public class TreeMap07 {
 class sort_key implements Comparator<String>{
 	@Override
 	public int compare(String str1, String str2) {
-		return 0;
+		return str1.compareTo(str2);
 	}
 }
