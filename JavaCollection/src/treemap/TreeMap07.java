8b4440d9991912c2b1bf53f4a58511fce2822ff1
yeonsu100 <yeonsu100@gmail.com>
2020-05-24 23:32:05 +0900

    TreeMap - 07

---
+++ JavaCollection/src/treemap/TreeMap07.java
@@ -0,0 +1,22 @@
+package treemap;
+import java.util.*;
+import java.util.Map.Entry;
+
+public class TreeMap07 {
+	public static void main(String[] args) {
+		TreeMap<String, String> tm=new TreeMap<String, String>();
+		tm.put("Frohana 1", "Elsa");
+		tm.put("Frohana 2", "Anna");
+		tm.put("Frohana 3", "Olaf");
+		tm.put("Frohana 4", "Jorgenbjorgen");
+		
+		System.out.println(tm);
+	}
+}
+
+class sort_key implements Comparator<String>{
+	@Override
+	public int compare(String str1, String str2) {
+		return 0;
+	}
+}
