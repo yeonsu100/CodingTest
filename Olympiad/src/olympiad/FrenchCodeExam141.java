4b1b728ea83de4cb155494744a55e4eb201aa067
yeonsu100 <yeonsu100@gmail.com>
2020-05-25 22:36:57 +0900

    add reverse method and while statement

--- Olympiad/src/olympiad/FrenchCodeExam141.java
+++ Olympiad/src/olympiad/FrenchCodeExam141.java
@@ -8,5 +8,20 @@ public class FrenchCodeExam141 {
 		int offset=3;
 		int len=A.length;
 		offset%=len;
+		
+		reverse(A, 0, len-offset-1);
+		reverse(A, len-offset, len-1);
+		reverse(A, 0, len-1);
+	}
+	
+	private static void reverse(char[] str, int start, int end) {
+		while(start<end) {
+			char temp=str[start];
+			str[start]=str[end];
+			str[end]=temp;
+			
+			start++;
+			end--;
+		}
 	}
 }
