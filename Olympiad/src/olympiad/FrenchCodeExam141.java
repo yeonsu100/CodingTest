8ed4a29749c4ba7578b1c2efad6674b850b7338f
yeonsu100 <yeonsu100@gmail.com>
2020-05-25 22:02:28 +0900

    FrenchCodeExam141 - main method and field

---
+++ Olympiad/src/olympiad/FrenchCodeExam141.java
@@ -0,0 +1,12 @@
+package olympiad;
+import java.util.*;
+
+public class FrenchCodeExam141 {
+	public static void main(String[] args) {
+		String str="Arendelle";
+		char[] A=str.toCharArray();
+		int offset=3;
+		int len=A.length;
+		offset%=len;
+	}
+}
