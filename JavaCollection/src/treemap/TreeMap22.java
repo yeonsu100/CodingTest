ed82cb0902416c095bfbdacf78f31b23d9ed6217
yeonsu100 <yeonsu100@gmail.com>
2020-05-28 19:31:10 +0900

    TreeMap - 22

---
+++ JavaCollection/src/treemap/TreeMap22.java
@@ -0,0 +1,22 @@
+package treemap;
+import java.util.*;
+import java.util.Map.Entry;
+
+public class TreeMap22 {
+	public static void main(String[] args) {
+		// Declare tree maps
+		TreeMap<Integer, String> tm=new TreeMap<Integer, String>();
+		SortedMap<Integer, String> sub_tm=new TreeMap<Integer, String>();
+		
+		// Put elements to the map
+		tm.put(10, "Elsa");
+		tm.put(20, "Anna");
+		tm.put(40, "Olaf");
+		tm.put(50, "Kristoff");
+		tm.put(60, "Sven");
+		
+		System.out.println("Original TreeMap content : "+tm);
+		sub_tm=tm.subMap(20, true, 50, true);
+		System.out.println("Sub map key-values : "+sub_tm);
+	}
+}
