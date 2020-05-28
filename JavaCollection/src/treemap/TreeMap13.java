0167677bbebae4cfa255ffe8dd0c38ca563f63a3
yeonsu100 <yeonsu100@gmail.com>
2020-05-28 19:29:33 +0900

    TreeMap - 13

---
+++ JavaCollection/src/treemap/TreeMap13.java
@@ -0,0 +1,21 @@
+package treemap;
+import java.util.*;
+
+public class TreeMap13 {
+	public static void main(String[] args) {
+		TreeMap<Integer, String> tm=new TreeMap<Integer, String>();
+		tm.put(10, "Elsa");
+		tm.put(20, "Anna");
+		tm.put(40, "Olaf");
+		tm.put(50, "Kristoff");
+		tm.put(60, "Sven");
+		
+		System.out.println("Original TreeMap content : "+tm);
+		System.out.println("Checking the entry for 10 : ");
+		System.out.println("Key(s) : "+tm.headMap(10));
+		System.out.println("Checking the entry for 30 : ");
+		System.out.println("Key(s) : "+tm.headMap(30));
+		System.out.println("Checking the entry for 70 : ");
+		System.out.println("Key(s) : "+tm.headMap(70));
+	}
+}
