8eb1cd64c2ba08a34b90369da559f98352070dd3
yeonsu100 <yeonsu100@gmail.com>
2020-06-01 14:58:17 +0900

    Bubble Sort

--- SortAlgorithm/src/sorting/BubbleSort.java
+++ SortAlgorithm/src/sorting/BubbleSort.java
@@ -1,5 +1,30 @@
 package sorting;
 
-public class BubbleSort {
+import java.util.Arrays;
 
+public class BubbleSort {
+	void bubbleSort(int nums[]) {
+		int n=nums.length;
+		for(int i=0; i<n-1; i++) {
+			for(int j=0; j<n-i-1; j++) {
+				if(nums[j]>nums[j+1]) {
+					// swap temp and nums[i]
+					int temp=nums[j];
+					nums[j]=nums[j+1];
+					nums[j+1]=temp;
+				}
+			}
+		}
+	}
+	
+	// Method to test above
+	public static void main(String[] args) {
+		BubbleSort ob=new BubbleSort();
+		int nums[]= {23, 7, -5, 3, 2, 1, -10, 0, -16, 45};
+		System.out.println("Original Arrays : ");
+		System.out.println(Arrays.toString(nums));
+		ob.bubbleSort(nums);
+		System.out.println("Sorted Array");
+		System.out.println(Arrays.toString(nums));
+	}
 }
