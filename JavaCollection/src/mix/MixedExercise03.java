1aa42ee630e51eec7dc6997abbce31593a0f69f3
yeonsu100 <yeonsu100@gmail.com>
2020-06-03 20:54:33 +0900

    Mixed Exercise - 03

---
+++ JavaCollection/src/mix/MixedExercise03.java
@@ -0,0 +1,33 @@
+package mix;
+import java.util.Arrays;
+
+public class MixedExercise03 {
+	static int[] rearrange(int[] new_array, int n) {
+		int temp[]=new int[n];
+		int small_num=0, large_num=n-1;
+		boolean flag=true;
+		
+		for(int i=0; i<n; i++) {
+			if(flag) {
+				temp[i]=new_array[large_num--];
+			}else {
+				temp[i]=new_array[small_num++];
+			}
+			flag = !flag;
+		}
+		return temp;
+	}
+	
+	public static void main(String[] args) {
+		int nums[]=new int[] {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+		int result[];
+		
+		System.out.println("Original Array : ");
+		System.out.println(Arrays.toString(nums));
+		
+		result=rearrange(nums, nums.length);
+		
+		System.out.println("New Array : ");
+		System.out.println(Arrays.toString(result));
+	}
+}
