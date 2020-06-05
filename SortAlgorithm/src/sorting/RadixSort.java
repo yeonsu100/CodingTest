cbf50cfa83920e0d43489b53598a04fdcd62fbb0
yeonsu100 <yeonsu100@gmail.com>
2020-06-05 18:35:23 +0900

    메소드 2개 추가

--- SortAlgorithm/src/sorting/RadixSort.java
+++ SortAlgorithm/src/sorting/RadixSort.java
@@ -2,6 +2,14 @@ package sorting;
 import java.util.Arrays;
 
 public class RadixSort {
+	public static void sort(int[] array) {
+		RadixSort.sort(array, 10);
+	}
+	
+	public static void sort(int[] array, int radix) {
+		
+	}
+	
 	public static void main(String[] args) {
 		RaidxSort ob=new RadixSort();
 		int nums[]= {7, -5, 3, 2, 1, 0, 45};
