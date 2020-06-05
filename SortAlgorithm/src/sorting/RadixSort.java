9ee688562ea7b6a906592e69ec536f831e4969fd
yeonsu100 <yeonsu100@gmail.com>
2020-06-05 19:50:45 +0900

    RadixSort - finished

--- SortAlgorithm/src/sorting/RadixSort.java
+++ SortAlgorithm/src/sorting/RadixSort.java
@@ -23,25 +23,50 @@ public class RadixSort {
 		}
 		
 		int exponent=1;
-		while((maxValue=minValue)/exponent >= 1) {
+		while((maxValue-minValue)/exponent >= 1) {
 			RadixSort.countingSortByDigit(array, radix, exponent, minValue);
             exponent *= radix;
 		}
 	}
 	
 	private static void countingSortByDigit(
-			int[] array, int radix, int exponent, int minvalue) {
+			int[] array, int radix, int exponent, int minValue) {
 		int bucketIndex;
 		int[] buckets=new int[radix];
 		int[] output=new int[array.length];
 		
+		for(int i=0; i<radix; i++) {
+			buckets[i]=0;
+		}
 		
+		for(int i=0; i<array.length; i++) {
+			bucketIndex=(int)(((array[i]-minValue)/exponent)%radix);
+			buckets[bucketIndex]++;
+		}
+		
+		for(int i=1; i<radix; i++) {
+			buckets[i]+=buckets[i-1];
+		}
+		
+		for(int i=array.length-1; i>=0; i--) {
+			bucketIndex=(int)(((array[i]-minValue)/exponent)%radix);
+			output[--buckets[bucketIndex]]=array[i];
+		}
+		
+		for(int i=0; i<array.length; i++) {
+			array[i]=output[i];
+		}
 	}
 	
 	public static void main(String[] args) {
-		RaidxSort ob=new RadixSort();
+		RadixSort ob=new RadixSort();
+		
 		int nums[]= {7, -5, 3, 2, 1, 0, 45};
 		System.out.println("Original Array : ");
+		System.out.println(Arrays.toString(nums));
+		
+		ob.sort(nums);
+		System.out.println("Sorted Array : ");
 		System.out.println(Arrays.toString(nums));
 	}
 }
