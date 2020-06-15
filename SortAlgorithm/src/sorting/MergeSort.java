4c3242e9333d035cd1086c371f7499cba273b8b3
yeonsu100 <yeonsu100@gmail.com>
2020-06-15 15:19:02 +0900

    Merge Sort - create a main method and merge sort

---
+++ SortAlgorithm/src/sorting/MergeSort.java
@@ -0,0 +1,9 @@
+package sorting;
+import java.util.Arrays;
+
+public class MergeSort {
+	public static void main(String[] args) {
+		MergeSort ob=new MergeSort();
+		int nums[]= {7, -5, 3, 2, 1, 0, 45};
+	}
+}
