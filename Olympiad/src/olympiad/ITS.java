6ef907ff215d74423f1f1d24b4f82093bfe99bf2
yeonsu100 <yeonsu100@gmail.com>
2020-07-11 21:55:22 +0900

    Increasing Triplet Subsequence

---
+++ Olympiad/src/olympiad/ITS.java
@@ -0,0 +1,20 @@
+package olympiad;
+
+public class ITS {
+	public boolean increasingTriplet(int[] nums) {
+	    int small=Integer.MAX_VALUE;
+	    int big=Integer.MAX_VALUE;
+	 
+	    for(int num:nums) {
+	        if(num<=small) {
+	            small=num;			// update x to be a smaller value
+	        }else if(num<=big){
+	            big=num; 			// update y to be a smaller value
+	        }else{
+	            return true;
+	        }
+	    }
+	 
+	    return false;
+	}
+}
