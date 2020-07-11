5c4bba30c3847569777afcf67fc6740adb5b23a3
yeonsu100 <yeonsu100@gmail.com>
2020-07-11 22:12:33 +0900

    One Edit Distance

---
+++ Olympiad/src/olympiad/OneEditDistance.java
@@ -0,0 +1,48 @@
+package olympiad;
+
+public class OneEditDistance {
+	public boolean isOneEditDistance(String s, String t) {
+	    if(s==null || t==null)
+	        return false;
+	 
+	    int m = s.length();
+	    int n = t.length();
+	 
+	    if(Math.abs(m-n)>1){
+	        return false;
+	    }
+	 
+	    int i=0; 
+	    int j=0; 
+	    int count=0;
+	 
+	    while(i<m&&j<n){
+	        if(s.charAt(i)==t.charAt(j)){
+	            i++;
+	            j++;
+	        }else{
+	            count++;
+	            if(count>1)
+	                return false;
+	 
+	            if(m>n){
+	                i++;
+	            }else if(m<n){
+	                j++;
+	            }else{
+	                i++;
+	                j++;
+	            }
+	        }
+	    }
+	 
+	    if(i<m||j<n){
+	        count++;
+	    }
+	 
+	    if(count==1)
+	        return true;
+	 
+	    return false;
+	}
+}
