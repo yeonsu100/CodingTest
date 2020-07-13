408c5d2a116b08e17c6fb1862160da2361aae885
yeonsu100 <yeonsu100@gmail.com>
2020-07-13 22:15:28 +0900

    Balanced Binary Tree

---
+++ JavaCollection/src/tree/BalancedBinaryTree.java
@@ -0,0 +1,39 @@
+package tree;
+
+class TreeNode{
+	int val;
+	TreeNode left;
+	TreeNode right;
+	
+	TreeNode(int x){
+		val=x;
+	}
+}
+
+public class BalancedBinaryTree {
+	public boolean isBalanced(TreeNode root) {
+		if(root==null) {
+			return true;
+		}
+		if(getHeight(root)==-1) {
+			return false;
+		}
+		return true;
+	}
+	
+	public int getHeight(TreeNode root) {
+		if(root==null) {
+			return 0;
+		}
+		int left=getHeight(root.left);
+		int right=getHeight(root.right);
+		
+		if(left==-1 || right==-1) {
+			return -1;
+		}
+		if(Math.abs(left-right) > 1) {
+			return -1;
+		}
+		return Math.max(left,  right)+1;
+	}
+}
