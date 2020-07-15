c8f8a3d1837cf24faf7698322448f0457abf4add
yeonsu100 <yeonsu100@gmail.com>
2020-07-15 22:20:00 +0900

    Closest Binary Search Tree Value

---
+++ JavaCollection/src/tree/ClosestBinarySearchTreeValue.java
@@ -0,0 +1,27 @@
+package tree;
+
+public class ClosestBinarySearchTreeValue {
+	int goal;
+    double min = Double.MAX_VALUE;
+ 
+    public int closestValue(TreeNode root, double target) {
+        helper(root, target);
+        return goal;
+    }
+ 
+    public void helper(TreeNode root, double target){
+        if(root==null)
+            return;
+ 
+        if(Math.abs(root.val - target) < min){
+            min=Math.abs(root.val-target);
+            goal=root.val;
+        } 
+ 
+        if(target < root.val){
+            helper(root.left, target);
+        }else{
+            helper(root.right, target);
+        }
+    }
+}
