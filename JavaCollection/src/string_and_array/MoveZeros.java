b29a2054dcd270bd6ada798dd9e50c6da04cd9f0
yeonsu100 <yeonsu100@gmail.com>
2020-07-28 18:12:41 +0900

    Move Zeros - 인덱스 저장

--- JavaCollection/src/string_and_array/MoveZeros.java
+++ JavaCollection/src/string_and_array/MoveZeros.java
@@ -3,7 +3,17 @@ package string_and_array;
 public class MoveZeros {
 	
 	public static void main(String[] args) {
+		// 1
 		int[] nums= {0, 3, 2, 0, 8, 5};
 		int index=0;
+		
+		// 2
+		for(int i=0; i<nums.length; i++) {
+			if(nums[i] != 0) {
+				nums[index]=nums[i];
+				index++;
+				System.out.println("index "+index);
+			}
+		}
 	}
 }
