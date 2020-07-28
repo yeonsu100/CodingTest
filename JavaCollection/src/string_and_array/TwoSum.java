02234c0fc5fc8c3d3c52ce09c6ed4aa0254c5fec
yeonsu100 <yeonsu100@gmail.com>
2020-07-28 18:43:34 +0900

    Two Sum - solved

--- JavaCollection/src/string_and_array/TwoSum.java
+++ JavaCollection/src/string_and_array/TwoSum.java
@@ -1,5 +1,8 @@
 package string_and_array;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TwoSum {
 	public static void main(String[] args) {
 		// 문제 
@@ -13,6 +16,19 @@ public class TwoSum {
 	}
 	
 	public int[] solve(int[] nums, int target) {
+		Map<Integer, Integer> map=new HashMap<>();
+		int[] result=new int[2];
+		
+		for(int i=0; i<nums.length; i++) {
+			// map에 값이 있는지 체크 
+			if(map.containsKey(nums[i])) {
+				int mapValue=map.get(nums[i]);	// i=1일 때 8, map(8,0)
+				result[0]=mapValue+1;		// 값 : 1
+				result[1]=i+1;				// 값 : 2
+			}else {
+				map.put(target-nums[i], i);		// key:10-2=8, value:i=0
+			}
+		}
 		
 		return nums;
 	}
