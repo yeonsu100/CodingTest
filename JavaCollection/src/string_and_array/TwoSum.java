136c46d2fc3a469f4bb56618ad129ccb40ec360d
yeonsu100 <yeonsu100@gmail.com>
2020-07-28 18:26:51 +0900

    Two Sum - 문제 정의 및 객체 생성

---
+++ JavaCollection/src/string_and_array/TwoSum.java
@@ -0,0 +1,19 @@
+package string_and_array;
+
+public class TwoSum {
+	public static void main(String[] args) {
+		// 문제 
+		int[] nums= {2, 8, 11, 21};
+		int target=10;
+		// output : int[]={1, 2};
+		
+		// 객체 생성하기 
+		TwoSum a=new TwoSum();
+		int[] result=a.solve(nums, target);
+	}
+	
+	public int[] solve(int[] nums, int target) {
+		
+		return nums;
+	}
+}
