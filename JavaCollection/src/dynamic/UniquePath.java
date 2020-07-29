75424e99a1b87e53ba18d41214e3dc8aced09450
yeonsu100 <yeonsu100@gmail.com>
2020-07-29 23:28:09 +0900

    Dynamic Programming을 이용한 Unique Path 문제 셋팅

--- JavaCollection/src/dynamic/UniquePath.java
+++ JavaCollection/src/dynamic/UniquePath.java
@@ -1,5 +1,13 @@
 package dynamic;
 
 public class UniquePath {
-
+	// Dynamic Programming
+	public static void main(String[] args) {
+		int m=7, n=3;
+		System.out.println(uniquePaths(m,n));
+	}
+	
+	public static int uniquePaths(int m, int n) {
+		
+	}
 }
