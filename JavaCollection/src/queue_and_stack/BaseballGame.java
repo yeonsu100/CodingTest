cffd7dd08fe08754fd9f8449c6b307eb166a1bd3
yeonsu100 <yeonsu100@gmail.com>
2020-07-29 23:07:45 +0900

    Stack을 이용한 BaseballGame - 문제 셋팅

---
+++ JavaCollection/src/queue_and_stack/BaseballGame.java
@@ -0,0 +1,12 @@
+package queue_and_stack;
+
+public class BaseballGame {
+	public static void main(String[] args) {
+		String[] strs = {"5","-2","4","C","D","9","+","+"};
+		System.out.println(points(strs));
+	}
+	
+	public static int points(String[] strs) {
+		
+	}
+}
