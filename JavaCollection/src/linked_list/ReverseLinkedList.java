af38fd0e045276ec607903578a3020a4bb8dc771
yeonsu100 <yeonsu100@gmail.com>
2020-07-29 23:00:42 +0900

    Reverse Linked List - solved

--- JavaCollection/src/linked_list/ReverseLinkedList.java
+++ JavaCollection/src/linked_list/ReverseLinkedList.java
@@ -1,28 +1,28 @@
 package linked_list;
 
-class ListNode{
+class ListNodee {
 	int val;
-	ListNode next;
-	ListNode(int x){
+	ListNodee next;
+	ListNodee(int x){
 		this.val = x;
 	}
 }
 
 public class ReverseLinkedList {
 	public static void main(String[] args) {
-		ListNode l1=new ListNode(1);
-		l1.next=new ListNode(1);
-		l1.next=new ListNode(2);
-		l1.next.next=new ListNode(3);
+		ListNodee l1=new ListNodee(1);
+		l1.next=new ListNodee(1);
+		l1.next=new ListNodee(2);
+		l1.next.next=new ListNodee(3);
 		
 		printNode(l1);
-		ListNode head=reverseList(l1);
+		ListNodee head=reverseList(l1);
 		printNode(head);
 	}
 	
-	public static ListNode reverseList(ListNode current) {
-		ListNode prev=null;
-		ListNode next=null;
+	public static ListNodee reverseList(ListNodee current) {
+		ListNodee prev=null;
+		ListNodee next=null;
 		
 		while(current != null) {
 			next=current.next;
@@ -31,5 +31,14 @@ public class ReverseLinkedList {
 			current=next;
 		}
 		return prev;
+	}
+	
+	public static void printNode(ListNodee head) {
+		System.out.println("print node : ");
+		while(head != null) {
+			System.out.println(head.val);
+			head=head.next;
+		}
+		System.out.println();
 	}
 }
