660812351bc099f2c2c6b46490d3c7915d01979c
yeonsu100 <yeonsu100@gmail.com>
2020-07-29 18:54:53 +0900

    merge 함수 생성해서 람다식으로 오름차순 구현

--- JavaCollection/src/string_and_array/MergeInterval.java
+++ JavaCollection/src/string_and_array/MergeInterval.java
@@ -12,6 +12,7 @@ class Interval {
 public class MergeInterval {
 	public static void main(String[] args) {
 		// int[] nums={{1,3}, {2,6}, {8,10}, {15,18}};
+		// sorting해야 하기 때문에 순서 바꿔서 작성 
 		Interval in2=new Interval(1,3);
 		Interval in1=new Interval(2,6);
 		Interval in3=new Interval(8,10);
@@ -28,4 +29,21 @@ public class MergeInterval {
 		
 		a.print(list);
 	}
+	
+	public List<Interval> merge(List<Interval> intervals){
+		if(intervals.isEmpty()) return intervals;
+		
+		// 1
+		List<Interval> result=new ArrayList<>();
+		
+		Collections.sort(intervals, (a,b)->a.start-b.start);	// 오름차순 (람다표현식 이용) 
+	}
+	
+	void print(List<Interval> list) {
+		for(int i=0; i<list.size(); i++) {
+			Interval in=list.get(i);
+			System.out.println(in.start+" "+in.end);
+		}
+	}
+	
 }
