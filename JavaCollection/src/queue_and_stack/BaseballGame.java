27e6d0e70a902284c787258ff73160afd2146183
yeonsu100 <yeonsu100@gmail.com>
2020-07-29 23:12:55 +0900

    case "C","D",default 생성

--- JavaCollection/src/queue_and_stack/BaseballGame.java
+++ JavaCollection/src/queue_and_stack/BaseballGame.java
@@ -1,5 +1,7 @@
 package queue_and_stack;
 
+import java.util.Stack;
+
 public class BaseballGame {
 	public static void main(String[] args) {
 		String[] strs = {"5","-2","4","C","D","9","+","+"};
@@ -7,6 +9,22 @@ public class BaseballGame {
 	}
 	
 	public static int points(String[] strs) {
+		// 데이터를 담을 공간 생성 
+		Stack<Integer> stack=new Stack<>();
 		
+		// 데이터 출력 
+		for(String op:strs) {
+			switch(op) {
+				case "C" : 
+					stack.pop();
+					break;
+				case "D" :
+					stack.push(stack.peek()*2);
+				case "+" : 
+					
+				default : 
+					stack.push(Integer.valueOf(op));
+			}
+		}
 	}
 }
