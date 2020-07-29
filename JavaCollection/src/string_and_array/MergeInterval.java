19bc6ebb476c615b77806affe90b133392820c96
yeonsu100 <yeonsu100@gmail.com>
2020-07-29 19:29:49 +0900

    람다식 이용

--- JavaCollection/src/string_and_array/MergeInterval.java
+++ JavaCollection/src/string_and_array/MergeInterval.java
@@ -5,58 +5,83 @@ import java.util.*;
 class Interval {
 	int start;
 	int end;
-	Interval() {start=0; end=0;}
-	Interval(int s, int e) {start=s; end=e;}
+
+	Interval() {
+		start = 0;
+		end = 0;
+	}
+
+	Interval(int s, int e) {
+		start = s;
+		end = e;
+	}
 }
 
 public class MergeInterval {
 	public static void main(String[] args) {
 		// int[] nums={{1,3}, {2,6}, {8,10}, {15,18}};
-		// sorting해야 하기 때문에 순서 바꿔서 작성 
-		Interval in2=new Interval(1,3);
-		Interval in1=new Interval(2,6);
-		Interval in3=new Interval(8,10);
-		Interval in4=new Interval(15,18);
-		
-		List<Interval> intervals=new ArrayList<>();
+		// sorting해야 하기 때문에 순서 바꿔서 작성
+		Interval in1 = new Interval(1, 3);
+		Interval in2 = new Interval(2, 6);
+		Interval in3 = new Interval(8, 10);
+		Interval in4 = new Interval(15, 18);
+
+		List<Interval> intervals = new ArrayList<>();
 		intervals.add(in1);
 		intervals.add(in2);
 		intervals.add(in3);
 		intervals.add(in4);
-		
-		MergeInterval a=new MergeInterval();
-		List<Interval> list=a.merge(intervals);
-		
+
+		MergeInterval a = new MergeInterval();
+		List<Interval> list = a.merge(intervals);
+		System.out.println("=== result ===");
 		a.print(list);
 	}
-	
-	public List<Interval> merge(List<Interval> intervals){
-		if(intervals.isEmpty()) return intervals;
-		
+
+	public List<Interval> merge(List<Interval> intervals) {
+		if (intervals.isEmpty())
+			return intervals;
+
 		// 1
-		List<Interval> result=new ArrayList<>();
-		
+		List<Interval> result = new ArrayList<>();
+
 		print(intervals);
-		Collections.sort(intervals, (a,b)->a.start-b.start);	// 오름차순 (람다표현식 이용) 
-	  //Collections.sort(intervals, (a,b)->b.start-a.start);	// 내림차순 
+		Collections.sort(intervals, (a, b) -> a.start - b.start); // 오름차순 (람다표현식 이용)
+		// Collections.sort(intervals, (a,b)->b.start-a.start); // 내림차순
 
 		System.out.println("=== after sorting ===");
 		print(intervals);
-		
+
+		// 2
+		Interval before = intervals.get(0); // [1,3]
+		for (int i = 1; i < intervals.size(); i++) {
+			Interval current = intervals.get(i);
+			if (before.end >= current.start) {
+				before.end = Math.max(before.end, current.end);
+			} else {
+				result.add(before);
+				before = current;
+			}
+		}
+
+		if (!result.contains(before)) {
+			result.add(before);
+		}
+
 		return result;
 	}
-	
-	void print(List<Interval> list) {
-		for(int i=0; i<list.size(); i++) {
-			Interval in=list.get(i);
-			System.out.println(in.start+" "+in.end);
-		}
-	}
-	
+
 	Comparator comp = new Comparator<Interval>() {
 		public int compare(Interval a, Interval b) {
 			// TODO Auto-generated method stub
 			return a.start - b.start;
 		}
 	};
+	
+	void print(List<Interval> list) {
+		for (int i = 0; i < list.size(); i++) {
+			Interval in = list.get(i);
+			System.out.println(in.start + " " + in.end);
+		}
+	}
 }
