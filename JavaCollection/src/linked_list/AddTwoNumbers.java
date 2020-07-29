3028d56a4985eeb1dc6c8d94cad0841f45e6626a
yeonsu100 <yeonsu100@gmail.com>
2020-07-29 19:51:20 +0900

    Add Two Numbers - solved

--- JavaCollection/src/linked_list/AddTwoNumbers.java
+++ JavaCollection/src/linked_list/AddTwoNumbers.java
@@ -18,11 +18,34 @@ public class AddTwoNumbers {
 		ListNode l2=new ListNode(5);
 		l2.next=new ListNode(6);
 		l2.next.next=new ListNode(2);
-		ListNode node=solve(l1, l2);
+		ListNode node=solve(l1, l2);		
 	}
 	
 	public static ListNode solve(ListNode l1, ListNode l2) {
+		// Dummy Data
+		ListNode newHead=new ListNode(0);
+		ListNode p1=l1, p2=l2, p3=newHead;
+		int carry=0;
 		
+		while(p1!=null || p2!=null) {
+			if(p1!=null) {
+				carry+=p1.val;
+				p1=p1.next;
+			}
+			
+			if(p2!=null) {
+				carry+=p2.val;
+				p2=p2.next;
+			}
+			
+			p3.next=new ListNode(carry%10);
+			p3=p3.next; 	// 넘긴 값을 다시 p3에 담는다 
+			carry /= 10;
+		}
+		
+		if(carry==1) p3.next=new ListNode(1);
+		
+		return newHead.next;
 	}
 	
 }
