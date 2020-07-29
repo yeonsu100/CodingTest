5145a6c10d914cda2fddf1186a1feb86ec2339ce
yeonsu100 <yeonsu100@gmail.com>
2020-07-29 17:57:25 +0900

    Daily Temperature - 문제 셋팅

---
+++ JavaCollection/src/string_and_array/DailyTemperature.java
@@ -0,0 +1,19 @@
+package string_and_array;
+
+import java.util.*;
+
+public class DailyTemperature {
+	public static void main(String[] args) {
+		int[] nums= {73, 74, 75, 71, 69, 72, 76, 73};
+		int[] res=dailyTemperatures(nums);
+		
+		System.out.println();
+		for(int i:res) {
+			System.out.print(i+" ");
+		}
+	}
+	
+	public static int[] dailyTemperatures(int[] temp) {
+		
+	}
+}
