09d25f23dc59f498d420debdaa5e231128ba9f0d
yeonsu100 <yeonsu100@gmail.com>
2020-07-29 18:03:57 +0900

    DailyTemperature - stack에 담고 빼내기

--- JavaCollection/src/string_and_array/DailyTemperature.java
+++ JavaCollection/src/string_and_array/DailyTemperature.java
@@ -7,13 +7,26 @@ public class DailyTemperature {
 		int[] nums= {73, 74, 75, 71, 69, 72, 76, 73};
 		int[] res=dailyTemperatures(nums);
 		
-		System.out.println();
+		System.out.println("=== result ===");
 		for(int i:res) {
 			System.out.print(i+" ");
 		}
 	}
 	
-	public static int[] dailyTemperatures(int[] temp) {
+	public static int[] dailyTemperatures(int[] temper) {
+		// 1. 스택에 담기 
+		Stack<Integer> stack=new Stack<>();
+		int[] result=new int[temper.length];
 		
+		// 2. pop 
+		for(int i=0; i<temper.length; i++) {
+			while(!stack.isEmpty() && temper[stack.peek()] < temper[i]) {
+				// result[0] = 1-0
+				int index=stack.pop();
+				result[index]=i-index;
+			}
+			
+			stack.push(i);
+		}
 	}
 }
