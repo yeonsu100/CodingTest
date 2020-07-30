828426e080e43e0237fad4bf25fca49e717207c5
yeonsu100 <yeonsu100@gmail.com>
2020-07-30 16:05:04 +0900

    Climbing Stairs - 문제 셋팅

---
+++ JavaCollection/src/dynamic/ClimbingStairs.java
@@ -0,0 +1,15 @@
+package dynamic;
+
+public class ClimbingStairs {
+	// Dynamic Programming
+	// dp[3]=dp[2]+dp[1]을 이용 
+	// 즉, dp[i]=dp[i-1]+dp[i-2]
+	public static void main(String[] args) {
+		int n=4;
+		System.out.println(climbStairs(n));
+	}
+	
+	public static int climbStairs(int n) {
+		
+	}
+}
