b5b0441508dd5dc3a6c002583d936e19a4ae24b5
yeonsu100 <yeonsu100@gmail.com>
2020-07-30 19:36:45 +0900

    NumberOfIsland_BFS - solved

--- JavaCollection/src/graph/NumberOfIsland_BFS.java
+++ JavaCollection/src/graph/NumberOfIsland_BFS.java
@@ -45,6 +45,11 @@ public class NumberOfIsland_BFS {
 				for(int[] dir : dirs) {
 					int x1=point[0]+dir[0];
 					int y1=point[1]+dir[1];
+					
+					if(x1>=0 && y1>=0 && x1<grid.length && y1<grid[0].length && grid[x][y]=='1') {
+						grid[x1][y1]='0';
+						queue.offer(new int[] {x1,y1});
+					}
 				}
 			}
 		}
