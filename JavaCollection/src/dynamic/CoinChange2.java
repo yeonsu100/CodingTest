5282c24a843d67faa3113c976251ecd51462e851
yeonsu100 <yeonsu100@gmail.com>
2020-07-30 15:56:28 +0900

    Coin Change2 - 데이터 담고 불러오기 완료

--- JavaCollection/src/dynamic/CoinChange2.java
+++ JavaCollection/src/dynamic/CoinChange2.java
@@ -1,5 +1,7 @@
 package dynamic;
 
+import java.util.Arrays;
+
 public class CoinChange2 {
 	// Dynamic Programming
 	public static void main(String[] args) {
@@ -9,6 +11,19 @@ public class CoinChange2 {
 	}
 	
 	public static int coinChange(int[] coins, int amount) {
+		// data 담기 
+		int max=amount+1;
+		int[] dp=new int[max];
+		Arrays.fill(dp, max);
+		dp[0]=0;
 		
+		// data 불러오기 
+		for(int i=1; i<=amount; i++) {
+			for(int j=0; j<coins.length; j++) {
+				if(i>=coins[j]) {
+					dp[i]=Math.min(dp[i], dp[i-coins[j]]+1);
+				}
+			}
+		}
 	}
 }
