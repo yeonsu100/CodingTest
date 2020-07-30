be31133658b02b2cc80b5da32f908a9a2d5ba0ee
yeonsu100 <yeonsu100@gmail.com>
2020-07-30 15:23:31 +0900

    Unique Path - solved

--- JavaCollection/src/dynamic/UniquePath.java
+++ JavaCollection/src/dynamic/UniquePath.java
@@ -8,6 +8,22 @@ public class UniquePath {
 	}
 	
 	public static int uniquePaths(int m, int n) {
+		Integer[][] map=new Integer[m][n];
 		
+		for(int i=0; i<m; i++) {
+			map[i][0]=1;
+		}
+		
+		for(int i=0; i<n; i++) {
+			map[0][i]=1;
+		}
+		
+		for(int i=1; i<m; i++) {
+			for(int j=1; j<n; j++) {
+				map[i][j]=map[i][j-1]+map[i-1][j];
+			}
+		}
+		
+		return map[m-1][n-1];
 	}
 }
