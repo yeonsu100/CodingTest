903c48242ed747ba2889ef67d5f6c47a93631c69
yeonsu100 <yeonsu100@gmail.com>
2020-07-30 19:02:00 +0900

    Num of island - 에러 처리 및 dfs 함수 추가

--- JavaCollection/src/graph/NumberOfIsland.java
+++ JavaCollection/src/graph/NumberOfIsland.java
@@ -15,13 +15,42 @@ public class NumberOfIsland {
 	
 	public int solve(char[][] grid) {
 		print(grid);
-		return 0;
+		
+		// error handling
+		if(grid==null || grid.length==0 || grid[0].length==0) return 0;
+		
+		// 00의 위치가 1인것부터 찾는다 
+		int count=0;
+		for(int i=0; i<grid.length; i++) {
+			for(int j=0; j<grid[i].length; j++) {
+				if(grid[i][j]==1) {
+					count++;
+					dfs(grid, i, j);
+				}
+			}
+		}
+		return count;
+	}
+	
+	public void dfs(char[][] grid, int i, int j) {
+		int m=grid.length;
+		int n=grid[0].length;
+		
+		if(i<0 || i>=m || j<0 || j>=n || grid[i][j]!='1') return;
+		grid[i][j]='X';			// 임의의 값 X를 넣는다 	
+		
+		dfs(grid, i-1, j);
+		dfs(grid, i+1, j);
+		dfs(grid, i, j-1);
+		dfs(grid, i, j+1);
+		
 	}
 	
 	public void print(char[][] grid) {
 		for(int i=0; i<grid.length; i++) {
 			for(int j=0; j<grid[i].length; j++) {
-				System.out.print(" grid["+i+"]["+j+"] "+grid[i][j]);
+			//	System.out.print(" grid["+i+"]["+j+"] "+grid[i][j]);
+				System.out.print(grid[i][j]);
 			}
 			System.out.println();
 		}
