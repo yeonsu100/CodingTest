acaf90f34bc6542525737a9e8b2e394639eac99d
yeonsu100 <yeonsu100@gmail.com>
2020-07-30 18:47:13 +0900

    Number Of Island - 문제 셋팅

---
+++ JavaCollection/src/graph/NumberOfIsland.java
@@ -0,0 +1,19 @@
+package graph;
+
+public class NumberOfIsland {
+	// DFS (Depth First Search) algorithm
+	public static void main(String[] args) {
+		char[][] grid={
+				{'1','1','1','0','1'},
+				{'1','1','0','0','0'},
+				{'1','1','0','0','1'},
+				{'0','0','0','0','1'}
+				};
+		NumberOfIsland a=new NumberOfIsland();
+		a.solve(grid);
+	}
+	
+	public int solve(char[][] grid) {
+		
+	}
+}
