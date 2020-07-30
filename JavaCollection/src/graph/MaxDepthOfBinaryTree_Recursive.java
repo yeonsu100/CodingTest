82c0eeef0fba30f559143d6911d1af7b6bc84880
yeonsu100 <yeonsu100@gmail.com>
2020-07-30 19:46:21 +0900

    MaxDepthOfBinaryTree_Recursive문제 셋팅 및 풀이전략 설정

---
+++ JavaCollection/src/graph/MaxDepthOfBinaryTree_Recursive.java
@@ -0,0 +1,31 @@
+package graph;
+/*
+ * 1. left, right를 호출한다 (null이 나오기 전까지 진행) 
+ * 2. Math.max(leftMax, rightMax)를 이용한다
+ */
+
+class TreeNode{
+	int val;
+	TreeNode left, right;
+	TreeNode(int x){
+		this.val=x;
+	}
+}
+
+public class MaxDepthOfBinaryTree_Recursive {
+	public static void main(String[] args) {
+		TreeNode tree=new TreeNode(3);
+		tree.left=new TreeNode(1);
+		tree.right=new TreeNode(4);
+		tree.left.left=new TreeNode(5);
+		tree.left.right=new TreeNode(8);
+		tree.left.left.left=new TreeNode(7);
+		MaxDepthOfBinaryTree_Recursive a=new MaxDepthOfBinaryTree_Recursive();
+		System.out.println(a.maxDepth(tree));
+	}
+	
+	public int maxDepth(TreeNode root) {
+		
+	}
+	
+}
