917825b7721f51ec7acf934129d2b0a8dd2868ee
yeonsu100 <yeonsu100@gmail.com>
2020-07-30 20:19:24 +0900

    MaxDepthOfBinaryTree_BFSqueue 큐 구현 및 pop 까지 완성

--- JavaCollection/src/graph/MaxDepthOfBinaryTree_BFSqueue.java
+++ JavaCollection/src/graph/MaxDepthOfBinaryTree_BFSqueue.java
@@ -1,4 +1,8 @@
 package graph;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
 /*
  * 1. queue에 넣어 근접해서 한칸씩 빼고 넣는 방식으로 접근한다  
  */
@@ -16,6 +20,21 @@ public class MaxDepthOfBinaryTree_BFSqueue {
 	}
 	
 	public int bfs(TreeNode root) {
+		// 1. Null check
+		if(root==null) return 0;
 		
+		// 2. 큐 구현 
+		Queue<TreeNode> queue=new LinkedList<>();
+		queue.offer(root);
+		int count=0;
+		
+		// 3. pop
+		while(!queue.isEmpty()) {
+			int size=queue.size();
+			for(int i=0; i<size; i++) {
+				TreeNode node=queue.poll();
+			}
+			count++;		// 빼낼 때마다 카운트를 하나씩 증가시킨다 
+		}
 	}
 }
