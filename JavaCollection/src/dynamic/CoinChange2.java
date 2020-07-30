daa600106538773bc70ec801855b65620d234c9c
yeonsu100 <yeonsu100@gmail.com>
2020-07-30 15:51:40 +0900

    Coin Change2 - 문제 셋팅

---
+++ JavaCollection/src/dynamic/CoinChange2.java
@@ -0,0 +1,14 @@
+package dynamic;
+
+public class CoinChange2 {
+	// Dynamic Programming
+	public static void main(String[] args) {
+		int[] coins= {1,2,5};
+		int amount=11;
+		System.out.println(coinChange(coins, amount));
+	}
+	
+	public static int coinChange(int[] coins, int amount) {
+		
+	}
+}
