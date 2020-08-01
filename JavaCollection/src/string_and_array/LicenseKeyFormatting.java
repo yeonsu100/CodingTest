f14540beb432f4ecd9f6f5147fc3c970b0514d69
yeonsu100 <yeonsu100@gmail.com>
2020-08-01 15:22:18 +0900

    조건 3 (네자리씩 끊기) 완료 (StringBuilder 이용)

--- JavaCollection/src/string_and_array/LicenseKeyFormatting.java
+++ JavaCollection/src/string_and_array/LicenseKeyFormatting.java
@@ -18,13 +18,28 @@ public class LicenseKeyFormatting {
 	}
 	
 	public static String solve(String str, int k) {
-		// 하이픈 없애기 (output : 8F3Z2e9wabcdef)
+		// 1. 하이픈 없애기 (output : 8F3Z2e9wabcdef)
 		String newStr=str.replace("-", "");	// (Char oldChar, Char newChar) 즉, -를 없애라는 의미
 		System.out.println("newStr1 : "+newStr);
 		
-		// 대문자로 변환 (output : 8F3Z2E9WABCDEF)
+		// 2. 대문자로 변환 (output : 8F3Z2E9WABCDEF)
 		newStr=newStr.toUpperCase();
 		System.out.println("newStr2 : "+newStr);
+		
+		// 3. 네자리씩 끊기 
+		int leng=newStr.length();		// 전체 길이 구하기 
+		System.out.println("leng : "+leng); 	// 14
+		// String을 StringBuilder로 변환한다 (메모리 및 멀티스레드 이슈때문에) 
+		StringBuilder sb=new StringBuilder();
+		for(int i=0; i<leng; i++) {
+			sb.append(newStr.charAt(i));
+		}
+		System.out.println("sb : "+sb);	// (output : 8F3Z2E9WABCDEF) 
+		for(int i=k; i<leng; i=i+k) {	// 4를 빼고, 8을 빼고, 12개를 빼낸다 
+			sb.insert(leng-i, '-');		// String이 아니라 Char이므로 싱글따옴표 사용 
+		}
+		System.out.println("-sb : "+sb);	// (output : 8F-3Z2E-9WAB-CDEF)
+
 
 		return newStr;
 	}
