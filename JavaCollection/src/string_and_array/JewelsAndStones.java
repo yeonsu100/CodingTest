d59547f56adbef0adfe89987f9c846010ea31dc6
yeonsu100 <yeonsu100@gmail.com>
2020-08-01 14:47:17 +0900

    JewelsAndStones - 문제 셋팅

---
+++ JavaCollection/src/string_and_array/JewelsAndStones.java
@@ -0,0 +1,18 @@
+package string_and_array;
+
+/*
+ * 1. 보석은 대소문자를 구분해서 갖고있어야 한다 
+ * 2. aA -> 2개 
+ * 3. 스톤에서 보석을 비교 (aA가 몇개였는지 체크)
+ *  * HashSet 이용 (중복 허용 X) 
+ */
+public class JewelsAndStones {
+	public static void main(String[] args) {
+		String J="aA", S="aAAbbbb";
+		System.out.println(solve(J,S));
+	}
+	
+	public static int solve(String jew, String stone) {
+		
+	}
+}
