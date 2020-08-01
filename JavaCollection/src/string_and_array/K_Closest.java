370db20a29754f4e2957527d307ed0a6f2e60e2e
yeonsu100 <yeonsu100@gmail.com>
2020-08-01 15:36:40 +0900

    K_Closest - 문제 셋팅

---
+++ JavaCollection/src/string_and_array/K_Closest.java
@@ -0,0 +1,23 @@
+package string_and_array;
+
+// Priority Queue 이용 
+public class K_Closest {
+	public static void main(String[] args) {
+		K_Closest a=new K_Closest();
+		int[][] points= {{1,3},{-2,2}};
+		int k=1;
+	//	int[][] points= {{3,3},{5,-1},{-2,4}};
+	//	int k=2;
+		int[][] result=a.solve(points,k){
+			a.print(result);
+		}
+		
+		public int[][] solve(int[][] points, int k){
+			
+		}
+		
+		public void print(int[][] result) {
+			
+		}
+	}
+}
