d14c15db8332500d570b89a2496aa3304144a608
yeonsu100 <yeonsu100@gmail.com>
2020-08-03 19:54:24 +0900

    K_Closest - solved (Priority Queue)

--- JavaCollection/src/string_and_array/K_Closest.java
+++ JavaCollection/src/string_and_array/K_Closest.java
@@ -1,5 +1,9 @@
 package string_and_array;
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 // Priority Queue 이용 
 public class K_Closest {
 	public static void main(String[] args) {
@@ -8,16 +12,42 @@ public class K_Closest {
 		int k=1;
 	//	int[][] points= {{3,3},{5,-1},{-2,4}};
 	//	int k=2;
-		int[][] result=a.solve(points,k){
+		int[][] result=a.solve(points,k);
 			a.print(result);
-		}
+	}
 		
-		public int[][] solve(int[][] points, int k){
-			
-		}
+	public int[][] solve(int[][] points, int k){
+		Queue<int[]> queue=new PriorityQueue<>(points.length, Comp);
+		int[][] result=new int[k][2];
+		int index=0;
 		
-		public void print(int[][] result) {
-			
+		for(int[] p:points) {
+			queue.offer(p);
+		}
+		while(index<k) {
+			result[index]=queue.poll();
+			index++;
+		}
+		return result;
+	}
+	
+	Comparator<int[]> Comp=new Comparator<int[]>() {
+		@Override
+		public int compare(int[] a, int[] b) {
+			// TODO Auto-generated method stub
+			return (a[0]*a[0]+a[1]+a[1])-(b[0]*b[0]+b[1]*b[1]);
+		}
+	};
+	
+	public void print(int[][] result) {
+		int m=result.length;
+		int n=result[0].length;
+		
+		for(int i=0; i<result.length; i++) {
+			for(int j=0; j<result[i].length; j++) {
+				System.out.print(result[i][j]);
+			}
+			System.out.println();
 		}
 	}
 }
