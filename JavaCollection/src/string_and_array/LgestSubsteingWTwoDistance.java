21861d535b44c990e4fff805e8aa74f91224f1bd
yeonsu100 <yeonsu100@gmail.com>
2020-08-06 16:19:10 +0900

    Longest Substring with at most Two distance - 문제 셋팅

---
+++ JavaCollection/src/string_and_array/LgestSubsteingWTwoDistance.java
@@ -0,0 +1,18 @@
+package string_and_array;
+
+import java.util.*;
+/* Longest Substring with at most Two distance 
+* input : String s="ccaabbb", output: 5
+* "aabbb" which its length is 5
+*/
+
+public class LgestSubsteingWTwoDistance {
+	public static void main(String[] args) {
+		String s="ccaabbb";
+		System.out.println(solve(s));
+	}
+	
+	public static int solve(String s) {
+		
+	}
+}
