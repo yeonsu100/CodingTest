f70d86088ce73545cd1dc74c27578534677d38d2
yeonsu100 <yeonsu100@gmail.com>
2020-08-06 15:26:12 +0900

    PlusOne - solved

--- JavaCollection/src/string_and_array/PlusOne.java
+++ JavaCollection/src/string_and_array/PlusOne.java
@@ -2,8 +2,8 @@ package string_and_array;
 
 public class PlusOne {
 	public static void main(String[] args) {
-		int[] digits= {1,2,3};
-	 // int[] digits= {9,9,9};
+	 //	int[] digits= {1,2,3};
+	    int[] digits= {9,9,9};
 		int[] result=plusOne(digits);
 		
 		for(int i:result) {
@@ -13,12 +13,20 @@ public class PlusOne {
 	
 	public static int[] plusOne(int[] digits) {
 		for(int i=digits.length-1; i>=0; i--) {
-			if(digits[] != 9) {
+			if(digits[i] != 9) {
 				digits[i]++;
 				break;
 			}else {
 				digits[i]=0;
 			}
+		}
+		
+		// 0,0,0 -> 1,0,0,0
+		if(digits[0]==0) {
+			// 증가시킨 뒤 값을 넣을 새로운 배열을 만든다 
+			int[] result=new int[digits.length+1];	// 4
+			result[0]=1;
+			return result;
 		}
 		return digits;
 	}
