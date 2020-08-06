ad220de3329f1afe32bf8655acd4cf7a66964722
yeonsu100 <yeonsu100@gmail.com>
2020-08-06 16:42:51 +0900

    Maximum Subarray - 문제 셋팅

---
+++ JavaCollection/src/string_and_array/MaximumSubarray.java
@@ -0,0 +1,19 @@
+package string_and_array;
+
+/*
+ * Subarray중에 합이 가장 큰 수를 return
+ * 1) 새로운 값=(새로운 값과 새로운 값+SubArray 값) 비교 
+ * 	  newSum=Math.max(새로운 값, 기존값과 합한 값)
+ * 	  Max=Math.max(newSum, max);
+ * 2) 1번에서 나온 max값을 따로 저장 (없을 경우 현재값이 max값) 
+ */
+public class MaximumSubarray {
+	public static void main(String[] args) {
+		int[] nums= {-2,1,-3,4,-1,2,1,-5,4};
+		System.out.println(maxSubArray(nums));
+	}
+	
+	public static int maxSubArray(int[] nums) {
+		
+	}
+}
