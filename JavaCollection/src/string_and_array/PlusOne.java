9273d6008284204afcdb94366d47abffada3b025
yeonsu100 <yeonsu100@gmail.com>
2020-08-06 14:26:04 +0900

    PlusOne - 문제 셋팅

---
+++ JavaCollection/src/string_and_array/PlusOne.java
@@ -0,0 +1,17 @@
+package string_and_array;
+
+public class PlusOne {
+	public static void main(String[] args) {
+		int[] digits= {1,2,3};
+	 // int[] digits= {9,9,9};
+		int[] result=plusOne(digits);
+		
+		for(int i:result) {
+			System.out.println("val : "+i);
+		}
+	}
+	
+	public static int[] plusOne(int[] digits) {
+		
+	}
+}
