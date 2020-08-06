15ca6e60b468050bc36cf09f43043f1fa3296256
yeonsu100 <yeonsu100@gmail.com>
2020-08-06 15:47:08 +0900

    Unique Email Address - 로컬주소와 도메인 주소 함수 생성, 풀이전략 노트

--- JavaCollection/src/string_and_array/UniqueEmailAddress.java
+++ JavaCollection/src/string_and_array/UniqueEmailAddress.java
@@ -1,6 +1,13 @@
 package string_and_array;
 
-// 주어진 3개의 문자열을 비교한 뒤 @ 이후의 문자열이 일치할 경우 같다고 출력 
+import java.util.HashSet;
+import java.util.Set;
+
+/* 주어진 3개의 문자열을 비교한 뒤 @ 이후의 문자열이 일치할 경우 같다고 출력 
+* 1) Continue로 뺀다
+* 2) + break로 뺀다
+* 3) Set<String>
+*/
 
 public class UniqueEmailAddress {
 	public static void main(String[] args) {
@@ -13,6 +20,19 @@ public class UniqueEmailAddress {
 	}
 	
 	public int numUniqueEmails(String[] emails) {
+		// 중복을 피하기 위해 Set<String>을 이용한다 
+		Set<String> set=new HashSet<>();
+		
+		for(String email: emails) {
+			String localName=makeLocalName(email);
+			String domainName=makeDomainName(email);
+		}
+	}
+	
+	private String makeLocalName(String email) {
+		
+	}
+	private String makeDomainName(String email) {
 		
 	}
 }
