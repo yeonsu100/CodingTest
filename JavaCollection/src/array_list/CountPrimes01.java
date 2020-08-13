6774b36a39105ec9809fa1675bc64f4cdad4a9fa
yeonsu100 <yeonsu100@gmail.com>
2020-08-13 21:05:37 +0900

    Count Primes 01 - solved

---
+++ JavaCollection/src/array_list/CountPrimes01.java
@@ -0,0 +1,34 @@
+package array_list;
+
+import java.util.ArrayList;
+
+public class CountPrimes01 {
+	public int countPrimes(int n) {
+		ArrayList<Integer> primes=new ArrayList<Integer>();
+		
+		if(n<=1)
+			return 0;
+		if(n==2)
+			return 1; 
+		if(n==3)
+			return 2;
+		
+		primes.add(2);
+		primes.add(3);
+		
+		for(int i=4; i<=n; i++) {
+			boolean isPrime=true;
+			for(int p:primes) {
+				int m=i%p;
+				if(m==0) {
+					isPrime=false;
+					break;
+				}
+			}
+			if(isPrime) {
+				primes.add(i);
+			}
+		}
+		return primes.size();
+	}
+}
