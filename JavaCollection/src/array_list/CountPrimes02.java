f7741e8f739cb66d58d7066004151b9b7e4ef10a
yeonsu100 <yeonsu100@gmail.com>
2020-08-13 21:34:35 +0900

    Count Primes 02 - for문 추가 (solved)

--- JavaCollection/src/array_list/CountPrimes02.java
+++ JavaCollection/src/array_list/CountPrimes02.java
@@ -10,6 +10,15 @@ public class CountPrimes02 {
 		for(int i=2; i<n; i++) {
 			primes[i]=true;
 		}
+		
+		for(int i=2; i<=Math.sqrt(n-1); i++) {
+	 // OR for (int i=2; i<=n-1; i++) {
+			if(primes[i]) {
+				for(int j=i+i; j<n; j+=i) {
+					primes[j]=false;
+				}
+			}
+		}
 
 		int count=0;
 		for(int i=2; i<n; i++) {
