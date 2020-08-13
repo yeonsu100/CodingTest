09c26dd57681161343aa7dce5d33afeed7d9ae6d
yeonsu100 <yeonsu100@gmail.com>
2020-08-13 22:10:43 +0900

    Trapping Rain Water - 함수 작성

---
+++ JavaCollection/src/string_and_array/TrappingRainWater.java
@@ -0,0 +1,38 @@
+package string_and_array;
+
+public class TrappingRainWater {
+	public int trap(int[] height) {
+		int result=0;
+		
+		if(height==null || height.length<=2)
+			return result;
+		
+		int left[] = new int[height.length];
+	    int right[]= new int[height.length];
+		
+		int max=height[0];
+		left[0]=height[0];
+		
+		for(int i=1; i<height.length; i++) {
+			if(height[i]<max) {
+				left[i]=max;
+			}else {
+				left[i]=height[i];
+				max=height[i];
+			}
+		}
+		
+		max=height[height.length-1];
+		right[height.length-1]=height[height.length-1];
+	    for(int i=height.length-2; i>=0; i--){
+	        if(height[i]<max){
+	            right[i]=max;
+	        }else {
+	            right[i]=height[i];
+	            max=height[i];
+	        }
+	    }
+	    
+	    return result;
+	}
+}
