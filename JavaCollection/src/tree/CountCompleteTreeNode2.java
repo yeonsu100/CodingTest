fbf62cb9691426e67ec29dd11dc61b6ddceddb80
yeonsu100 <yeonsu100@gmail.com>
2020-08-14 22:46:36 +0900

    Count Complete TreeNode2 - L/R 추가 (solved)

--- JavaCollection/src/tree/CountCompleteTreeNode2.java
+++ JavaCollection/src/tree/CountCompleteTreeNode2.java
@@ -14,4 +14,26 @@ public class CountCompleteTreeNode2 {
 	        return countNodes(root.left)+countNodes(root.right)+1;
 	    }
 	}
+	
+	public int getLeftHeight(TreeNode n){
+	    if(n==null) return 0;
+	 
+	    int height=0;
+	    while(n.left!=null){
+	        height++;
+	        n=n.left;
+	    }
+	    return height;
+	}
+	 
+	public int getRightHeight(TreeNode n){
+	    if(n==null) return 0;
+	 
+	    int height=0;
+	    while(n.right!=null){
+	        height++;
+	        n=n.right;
+	    }
+	    return height;
+	}
 }
