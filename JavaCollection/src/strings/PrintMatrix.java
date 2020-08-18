8cd43656aa096efbb45ecca5f72a940a8c4585e3
yeonsu100 <yeonsu100@gmail.com>
2020-08-18 22:19:27 +0900

    Print Matrix - solved

--- JavaCollection/src/strings/PrintMatrix.java
+++ JavaCollection/src/strings/PrintMatrix.java
@@ -6,10 +6,17 @@ public class PrintMatrix {
 	public static void main(String[] args) {
 		Scanner in=new Scanner(System.in);
 		System.out.print("Input a number : ");
-		
+		int n=in.nextInt();
+		printMatrix(n);
 	}
 	
 	public static void printMatrix(int n) {
-		
+		// for문 돌려서 출력 
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<n; j++) {
+				System.out.print((int)(Math.random()*2)+" ");
+			}
+			System.out.println();
+		}
 	}
 }
