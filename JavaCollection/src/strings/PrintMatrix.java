4fd91a9d1dc5db3ea3889a5f778e8bdb9004adde
yeonsu100 <yeonsu100@gmail.com>
2020-08-18 22:16:12 +0900

    Print Matrix - 문제 셋팅

---
+++ JavaCollection/src/strings/PrintMatrix.java
@@ -0,0 +1,15 @@
+package strings;
+
+import java.util.Scanner;
+
+public class PrintMatrix {
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		System.out.print("Input a number : ");
+		
+	}
+	
+	public static void printMatrix(int n) {
+		
+	}
+}
