eba816c546e3dfdfa94778ef8f59fabb13137f9f
yeonsu100 <yeonsu100@gmail.com>
2020-08-30 18:29:51 +0900

    Remove Duplicates from Sorted List - 셋킹

---
+++ JavaCollection/src/linked_list/RemoveDuplicates01.java
@@ -0,0 +1,20 @@
+package linked_list;
+
+/*
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+
+public class RemoveDuplicates01 {
+	public ListNode deleteDuplicates(ListNode head) {
+		if(head==null || head.next==null) 
+			return head;
+	}
+}
