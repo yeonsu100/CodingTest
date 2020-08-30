5ee649f7d72c328a5114b83bfba5c29f3be94a5d
yeonsu100 <yeonsu100@gmail.com>
2020-08-30 18:16:38 +0900

    Partition List - while문

--- JavaCollection/src/linked_list/PartitionList.java
+++ JavaCollection/src/linked_list/PartitionList.java
@@ -6,6 +6,19 @@ public class PartitionList {
 		
 		ListNode fakeHead1=new ListNode(0);
 		ListNode fakeHead2=new ListNode(0);
+		fakeHead1.next=head;
+		
+		ListNode p=head;
+		ListNode prev=fakeHead1;
+		ListNode p2=fakeHead2;
+		
+		while(p!=null) {
+			if(p.val < x) {
+				p=p.next;
+			}else {
+				p2.next=p;
+			}
+		}
 
 		return fakeHead1.next;
 	}
