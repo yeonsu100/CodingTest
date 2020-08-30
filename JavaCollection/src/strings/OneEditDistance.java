57f5ef06ff411abaecce4f9140aebc1c211692b2
yeonsu100 <yeonsu100@gmail.com>
2020-08-30 17:20:38 +0900

    One Edit Distance - 문제 셋팅

---
+++ JavaCollection/src/strings/OneEditDistance.java
@@ -0,0 +1,11 @@
+package strings;
+
+public class OneEditDistance {
+	public boolean isOneEditDistance(String s, String t) {
+		if(s==null || t==null)
+			return false;
+		
+		int m=s.length();
+		int n=t.length();
+	}
+}
