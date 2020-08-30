2da763ec5b4f6103bf08c232342fab40e4fcfd60
yeonsu100 <yeonsu100@gmail.com>
2020-08-30 18:03:30 +0900

    Partition List - 문제 셋팅

---
+++ JavaCollection/src/linked_list/PartitionList.java
@@ -0,0 +1,12 @@
+package linked_list;
+
+public class PartitionList {
+	public ListNode partiton(ListNode head, int x) {
+		if(head==null) return null;
+		
+		ListNode fakeHead1=new ListNode(0);
+		ListNode fakeHead2=new ListNode(0);
+
+		return fakeHead1.next;
+	}
+}
