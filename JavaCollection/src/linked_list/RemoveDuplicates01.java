9b599c8aa86ff30d8e06809d68a364bbeb9cada8
yeonsu100 <yeonsu100@gmail.com>
2020-09-01 23:18:52 +0900

    Remove Duplicates 01 solved

--- JavaCollection/src/linked_list/RemoveDuplicates01.java
+++ JavaCollection/src/linked_list/RemoveDuplicates01.java
@@ -16,5 +16,21 @@ public class RemoveDuplicates01 {
 	public ListNode deleteDuplicates(ListNode head) {
 		if(head==null || head.next==null) 
 			return head;
+		
+		ListNode prev=head;
+		ListNode p=head.next;
+		
+		while(p!=null) {
+			if(p.val==prev.val) {
+				prev.next=p.next;
+				p=p.next;
+				// no charge prev
+			}else {
+				prev=p;
+				p=p.next;
+			}
+		}
+		
+		return head;
 	}
 }
