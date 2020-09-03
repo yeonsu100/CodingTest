6b5f9184f84b8197442ea65a4a9047e5993c96cd
yeonsu100 <yeonsu100@gmail.com>
2020-09-03 23:12:53 +0900

    Stooge sort - 문제 셋팅

---
+++ SortAlgorithm/src/sorting/StoogeSort.java
@@ -0,0 +1,18 @@
+package sorting;
+
+import java.io.*;
+
+public class StoogeSort {
+    static void stoogesort(int arr[], int l, int h) { 
+        if (l>=h) return; 
+  
+        if (arr[l]>arr[h]) { 
+            int t=arr[l]; 
+            arr[l]=arr[h]; 
+            arr[h]=t; 
+        } 
+  
+    public static void main(String args[]) { 
+        
+    } 
+}
