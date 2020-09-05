6dcd4902b2bd3d981cb241f9d733c6f94672438a
yeonsu100 <yeonsu100@gmail.com>
2020-09-05 22:21:40 +0900

    GFS - 셋팅

---
+++ JavaCollection/src/graph/GFS02.java
@@ -0,0 +1,23 @@
+package graph;
+
+import java.io.*;
+
+public class GFS02 {
+	static void pattern(int line) { 
+        int i, j; 
+        for(i = 0; i<line; i++) { 
+            for(j = 0; j<line; j++) { 
+                if((j==1 && i!=0 && i!=line-1) || 
+                ((i==0 || i==line-1) && j>1 && 
+                j<line-2) || (i==((line-1)/2) &&  
+                j>2 && j<line-1) || (j == line-2 &&  
+                i!=0 && i>=((line-1)/2) && i!=line-1)) 
+                    System.out.print("*"); 
+                else
+                    System.out.print(" "); 
+      
+            } 
+            System.out.println(); 
+        } 
+    } 
+}
