dc9a1b0ffae5dd7bbfbd4154a94ca5616be00f7d
yeonsu100 <yeonsu100@gmail.com>
2020-09-05 21:37:16 +0900

    Word Distance - 문제 셋팅

---
+++ JavaCollection/src/hashmap/WordDistance.java
@@ -0,0 +1,11 @@
+package hashmap;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class WordDistance {
+	HashMap<String, ArrayList<Integer>> map;
+	public WordDistance(String[] words) {
+		map=new HashMap<String, ArrayList<Integer>>();
+	}
+}
