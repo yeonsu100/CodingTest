a09b3b2211e67fa8ee4b24d42033be6b3f6fb579
yeonsu100 <yeonsu100@gmail.com>
2020-09-06 22:11:32 +0900

    MixedExercise10 - searchInsert 추가

--- JavaCollection/src/mix/MixedExercise10.java
+++ JavaCollection/src/mix/MixedExercise10.java
@@ -6,5 +6,18 @@ public class MixedExercise10 {
 		int[] nums= {1, 3, 7, 8, 10, 13};
 		int target;
 		target=7;
+		
+		System.out.print(searchInsert(nums, target));
+
+	}
+	
+	public static int searchInsert(int[] nums1, int target) {
+		if(nums1==null || nums1.length==0) {
+			return 0;
+		}
+		int start=0;
+		int end=nums1.length-1;
+		int mid=start+(end-start)/2;
+
 	}
 }
