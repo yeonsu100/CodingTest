c62782063dc82c02a3a4ef1a4ee3f23b64ce6da9
yeonsu100 <yeonsu100@gmail.com>
2020-09-06 22:05:21 +0900

    MixedExercise10 - 문제 셋팅

---
+++ JavaCollection/src/mix/MixedExercise10.java
@@ -0,0 +1,10 @@
+package mix;
+import java.util.*;
+
+public class MixedExercise10 {
+	public static void main(String[] args) {
+		int[] nums= {1, 3, 7, 8, 10, 13};
+		int target;
+		target=7;
+	}
+}
