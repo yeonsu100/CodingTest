bc72ddb564a65c640f4e9e8fb3a0294c71522709
yeonsu100 <yeonsu100@gmail.com>
2020-09-06 21:38:40 +0900

    MixedExercise09 문제셋팅

---
+++ JavaCollection/src/mix/MixedExercise09.java
@@ -0,0 +1,19 @@
+package mix;
+
+public class MixedExercise09 {
+	static boolean sum_pair(int arr_int[], int n, int x) {
+		int k;
+		for(k=0; k<n-1; k++) {
+			if(arr_int[k]>arr_int[k+1]) {
+				break;
+			}
+		}
+		int l=(k+1)%n;
+		int r=k;
+		
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+}
