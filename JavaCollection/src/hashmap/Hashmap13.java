c8c7b221b1dd2048d1e8a78c91abe6f7e2f32131
yeonsu100 <yeonsu100@gmail.com>
2020-09-07 22:43:56 +0900

    Hashmap example - 주석 추가

--- JavaCollection/src/hashmap/Hashmap13.java
+++ JavaCollection/src/hashmap/Hashmap13.java
@@ -10,19 +10,24 @@ public class Hashmap13 {
 		hm.put("Kristoff", 4);
 		hm.put("Sven", 5);
 		
+		// print the original map
 		System.out.println("The Original map : "+hm);
 		
 		System.out.println("\n1. Is key 'Olaf' exists?");
 		if(hm.containsKey("Olaf")) {
+			// key exists
 			System.out.println("Yes, "+hm.get("Olaf"));
 		}else {
+			// key does NOT exists
 			System.out.println("No.");
 		}
 		
 		System.out.println("\n2. Is key 'Mattias' exists?");
 		if(hm.containsKey("Mattias")) {
+			// key exists
 			System.out.println("Yes, "+hm.get("Mattias"));
 		}else {
+			// key does NOT exists
 			System.out.println("No.");
 		}
 	}
