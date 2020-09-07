b634be05ab757388b94f88a3cf8edabcbd444b8d
yeonsu100 <yeonsu100@gmail.com>
2020-09-07 22:35:22 +0900

    CountPrimes03 - if문 추가

--- JavaCollection/src/array_list/CountPrimes03.java
+++ JavaCollection/src/array_list/CountPrimes03.java
@@ -6,6 +6,13 @@ public class CountPrimes03 {
 	public int countPrimes(int n) {
 		ArrayList<Integer> primes=new ArrayList<Integer>();
 		
+		if(n<=1)
+			return 0;
+		if(n==2)
+			return 1; 
+		if(n==3)
+			return 2;
+		
 		return false;
 	}
 }
