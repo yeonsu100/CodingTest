2662e7d4ffe478af77e4a6ac9b223cadee40b3d5
yeonsu100 <yeonsu100@gmail.com>
2020-09-07 22:34:14 +0900

    Count Primes - 문제 셋팅

---
+++ JavaCollection/src/array_list/CountPrimes03.java
@@ -0,0 +1,11 @@
+package array_list;
+
+import java.util.ArrayList;
+
+public class CountPrimes03 {
+	public int countPrimes(int n) {
+		ArrayList<Integer> primes=new ArrayList<Integer>();
+		
+		return false;
+	}
+}
