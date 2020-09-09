7dbe93837ff6525ed793220012304f92b1c04550
yeonsu100 <yeonsu100@gmail.com>
2020-09-09 23:47:05 +0900

    ClosestIn3 - 문제 셋팅, 임의의 리턴값

---
+++ JavaCollection/src/array_list/ClosestIn3.java
@@ -0,0 +1,14 @@
+package array_list;
+
+import java.util.Arrays;
+
+public class ClosestIn3 {
+	public int closest(int[] nums, int target) {
+	    int min=Integer.MAX_VALUE;
+		int result=0;
+	 
+		Arrays.sort(nums);
+	 
+		return 0;
+	}
+}
