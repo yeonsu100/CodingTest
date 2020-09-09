b198e9a05ec8750775da5493480ba94ea68e6974
yeonsu100 <yeonsu100@gmail.com>
2020-09-10 00:11:17 +0900

    ClosestIn3 - while문, 조건 추가

--- JavaCollection/src/array_list/ClosestIn3.java
+++ JavaCollection/src/array_list/ClosestIn3.java
@@ -12,6 +12,24 @@ public class ClosestIn3 {
 		for(int i=0; i<nums.length; i++) {
 			int j=i+1;
 			int k=nums.length-1;
+			
+			while (j<k) {
+				int sum=nums[i]+nums[j]+nums[k];
+				int diff=Math.abs(sum-target);
+	 
+				if(diff==0) 
+					return sum;
+	 
+				if(diff<min) {
+					min=diff;
+					result=sum;
+				}
+				if(sum<=target) {
+					j++;
+				}else{
+					k--;
+				}
+			}
 		}
 		return 0;
 	}
