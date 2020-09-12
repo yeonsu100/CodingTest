7ec80f4187e02a8ff746ceaf54e26920cd930c24
yeonsu100 <yeonsu100@gmail.com>
2020-09-12 16:43:46 +0900

    Permutations02 - 주석 추가

--- JavaCollection/src/permutations/Permutations02.java
+++ JavaCollection/src/permutations/Permutations02.java
@@ -6,18 +6,23 @@ public class Permutations02 {
 	public ArrayList<ArrayList<Integer>> permute(int[] num){
 		ArrayList<ArrayList<Integer>> result=new ArrayList<ArrayList<Integer>>();
 		
+		// start from an empty list
 		result.add(new ArrayList<Integer>());
 		
 		for(int i=0; i<num.length; i++) {
+			// list of list in currrent iteration of the array num
 			ArrayList<ArrayList<Integer>> current=new ArrayList<ArrayList<Integer>>();
 			
 			for(ArrayList<Integer> l:result) {
+				// # of locations to insert is largest index + 1
 				for(int j=0; j<l.size()+1; j++) {
+					// + add num[i] to different locations
 					l.add(j, num[i]);
 					
 					ArrayList<Integer> temp=new ArrayList<Integer>(l);
 					current.add(temp);
 
+					// - remove num[i] add
 					l.remove(j);
 				}
 			}
