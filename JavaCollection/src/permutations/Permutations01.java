68540bb7937f2eb02f73b98d75d0c0103e8454fa
yeonsu100 <yeonsu100@gmail.com>
2020-09-12 16:08:09 +0900

    Permutations01 - for문, swap 추가

--- JavaCollection/src/permutations/Permutations01.java
+++ JavaCollection/src/permutations/Permutations01.java
@@ -13,10 +13,21 @@ public class Permutations01 {
 	private void helper(int start, int[] nums, List<List<Integer>> result) {
 		if(start==nums.length) {
 			ArrayList<Integer> list=new ArrayList<>();
+				for(int num:nums) {
+					list.add(num);
+				}
 			result.add(list);
 			return;
 		}
 		
+		for(int i=start; i<nums.length; i++) {
+			swap(nums, i, start);
+			helper(start, nums, result);
+			swap(nums, i, start);
+		}
+	}
+	
+	private void swap(int[] nums, int i, int j) {
 		
 	}
 }
