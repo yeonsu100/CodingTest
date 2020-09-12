8ba55d3731ef429cc114633ce62f52c7f864ae4c
yeonsu100 <yeonsu100@gmail.com>
2020-09-12 16:03:00 +0900

    Permutations01 - helper 추가

--- JavaCollection/src/permutations/Permutations01.java
+++ JavaCollection/src/permutations/Permutations01.java
@@ -5,7 +5,18 @@ import java.util.*;
 public class Permutations01 {
 	public List<List<Integer>> permute(int[] nums){
 		List<List<Integer>> result=new ArrayList<>();
+		helper(0, nums, result);
 
 		return result;
+	}
+	
+	private void helper(int start, int[] nums, List<List<Integer>> result) {
+		if(start==nums.length) {
+			ArrayList<Integer> list=new ArrayList<>();
+			result.add(list);
+			return;
+		}
+		
+		
 	}
 }
