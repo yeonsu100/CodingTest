29cce029325bb82baf54540c94073231da042faa
yeonsu100 <yeonsu100@gmail.com>
2020-09-12 16:18:38 +0900

    Permutations02 - setting

---
+++ JavaCollection/src/permutations/Permutations02.java
@@ -0,0 +1,11 @@
+package permutations;
+
+import java.util.*;
+
+public class Permutations02 {
+	public ArrayList<ArrayList<Integer>> permute(int[] num){
+		ArrayList<ArrayList<Integer>> result=new ArrayList<ArrayList<Integer>>();
+		
+		return result;
+	}
+}
