12983ed77f4b29b4365fc4a619248ab096d89b83
yeonsu100 <yeonsu100@gmail.com>
2020-09-12 15:57:32 +0900

    Permutations01 - 셋팅

---
+++ JavaCollection/src/permutations/Permutations01.java
@@ -0,0 +1,11 @@
+package permutations;
+
+import java.util.*;
+
+public class Permutations01 {
+	public List<List<Integer>> permute(int[] nums){
+		List<List<Integer>> result=new ArrayList<>();
+
+		return result;
+	}
+}
