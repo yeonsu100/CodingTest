a77a00e9e82e463dc004948e3c75923f51d724f0
yeonsu100 <yeonsu100@gmail.com>
2020-09-12 16:53:50 +0900

    FactorCombinations01 - setting

---
+++ JavaCollection/src/combinations/FactorCombinations01.java
@@ -0,0 +1,11 @@
+package combinations;
+
+import java.util.*;
+
+public class FactorCombinations01 {
+	public List<List<Integer>> getFactors(int n){
+		List<List<Integer>> result=new ArrayList<List<Integer>>();
+		
+		return result;
+	}
+}
