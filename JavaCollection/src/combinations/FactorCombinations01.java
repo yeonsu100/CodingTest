2317b412fceb2b4881707ea366d69e07cc85a5e1
yeonsu100 <yeonsu100@gmail.com>
2020-09-12 16:58:59 +0900

    FactorCombinations01 - helper 메소드 추가

--- JavaCollection/src/combinations/FactorCombinations01.java
+++ JavaCollection/src/combinations/FactorCombinations01.java
@@ -6,6 +6,14 @@ public class FactorCombinations01 {
 	public List<List<Integer>> getFactors(int n){
 		List<List<Integer>> result=new ArrayList<List<Integer>>();
 		
+		List<Integer> list=new ArrayList<Integer>();
+	    helper(2,1,n,result,list);
+	    
 		return result;
+	}
+	
+	public void helper(int start, int product, int n, 
+			List<List<Integer>> result, List<Integer> curr){
+		
 	}
 }
