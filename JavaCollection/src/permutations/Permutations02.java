364b2e79a88913a1796eb9752ad43243ddc731df
yeonsu100 <yeonsu100@gmail.com>
2020-09-12 16:31:19 +0900

    Permutations02 - 메소드, for문 추ㅏ

--- JavaCollection/src/permutations/Permutations02.java
+++ JavaCollection/src/permutations/Permutations02.java
@@ -6,6 +6,18 @@ public class Permutations02 {
 	public ArrayList<ArrayList<Integer>> permute(int[] num){
 		ArrayList<ArrayList<Integer>> result=new ArrayList<ArrayList<Integer>>();
 		
+		result.add(new ArrayList<Integer>());
+		
+		for(int i=0; i<num.length; i++) {
+			ArrayList<ArrayList<Integer>> current=new ArrayList<ArrayList<Integer>>();
+			
+			for(ArrayList<Integer> l:result) {
+				for(int j=0; j<l.size()+1; j++) {
+					l.add(j, num[i]);
+				}
+			}
+		}
+		
 		return result;
 	}
 }
