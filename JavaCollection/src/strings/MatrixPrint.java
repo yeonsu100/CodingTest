684d42757961be9a5afdb9cdf2482d8e0c8826dd
yeonsu100 <yeonsu100@gmail.com>
2020-09-13 23:05:39 +0900

    Matrix Print - 문제 셋팅

---
+++ JavaCollection/src/strings/MatrixPrint.java
@@ -0,0 +1,14 @@
+package strings;
+
+import java.util.Scanner;
+
+public class MatrixPrint {
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		System.out.print("Input a number : ");
+	}
+	
+	public static void printMatrix(int n) {
+		
+	}
+}
