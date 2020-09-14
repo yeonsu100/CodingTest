970cda0a35552ebb054f94bad7a5f81451080d75
yeonsu100 <yeonsu100@gmail.com>
2020-09-14 22:29:28 +0900

    FourSum - not solved yet

--- JavaCollection/src/array_list/FourSum.java
+++ JavaCollection/src/array_list/FourSum.java
@@ -9,5 +9,34 @@ public class FourSum {
 	    if(nums==null|| nums.length<4)
 	        return result;
 	 
+	    Arrays.sort(nums);
+	    
+	    for(int i=0; i<nums.length-3; i++){
+	        if(i!=0 && nums[i]==nums[i-1])
+	            continue;
+	        for(int j=i+1; j<nums.length-2; j++){
+	            if(j!=i+1 && nums[j]==nums[j-1])
+	                continue;
+	            int k=j+1;
+	            int l=nums.length-1;
+	            while(k<l){
+	                if(nums[i]+nums[j]+nums[k]+nums[l]<target){
+	                    k++;
+	                }else if(nums[i]+nums[j]+nums[k]+nums[l]>target){
+	                    l--;
+	                }else{
+	                    List<Integer> t = new ArrayList<Integer>();
+	                    t.add(nums[i]);
+	                    t.add(nums[j]);
+	                    t.add(nums[k]);
+	                    t.add(nums[l]);
+	                    result.add(t);
+	 
+	                    k++;
+	                    l--;
+	                }
+	            }
+	        }
+	    }
 	}
 }
