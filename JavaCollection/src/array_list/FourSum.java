aa8c8789350c0f30c51660bc3b73326245cce1cf
yeonsu100 <yeonsu100@gmail.com>
2020-09-14 22:24:08 +0900

    FourSum - 초기 셋팅

---
+++ JavaCollection/src/array_list/FourSum.java
@@ -0,0 +1,13 @@
+package array_list;
+
+import java.util.*;
+
+public class FourSum {
+	public List<List<Integer>> fourSum(int[] nums, int target) {
+	    List<List<Integer>> result=new ArrayList()<List<Integer>>();
+	 
+	    if(nums==null|| nums.length<4)
+	        return result;
+	 
+	}
+}
