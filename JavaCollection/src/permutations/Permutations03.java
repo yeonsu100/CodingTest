98303ea19b1e7f0dbca234c85a5715e68f324cc4
yeonsu100 <yeonsu100@gmail.com>
2020-09-15 22:43:08 +0900

    Permutations03 - helper ㅔ소드 추가

--- JavaCollection/src/permutations/Permutations03.java
+++ JavaCollection/src/permutations/Permutations03.java
@@ -5,6 +5,19 @@ import java.util.*;
 public class Permutations03 {
 	public List<List<Integer>> permuteUnique(int[] nums) {
 	    List<List<Integer>> result = new ArrayList<>();
+	    
+	    helper(0, nums, result);
 	    return result;
+	}
+	
+	private void helper(int start, int[] nums, List<List<Integer>> result){
+	    if(start==nums.length-1){
+	        ArrayList<Integer> list=new ArrayList<>();
+	        for(int num: nums){
+	            list.add(num);
+	        }
+	        result.add(list);
+	        return;
+	    }
 	}
 }
