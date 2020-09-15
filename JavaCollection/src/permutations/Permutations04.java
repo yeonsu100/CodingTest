fef4e2523af663def0eb94e9c7b58cb259c9da69
yeonsu100 <yeonsu100@gmail.com>
2020-09-15 23:05:45 +0900

    Permutations04 - for문, hashset 추가

--- JavaCollection/src/permutations/Permutations04.java
+++ JavaCollection/src/permutations/Permutations04.java
@@ -1,11 +1,15 @@
 package permutations;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class Permutations04 {
 	public static ArrayList<ArrayList<Integer>> permuteUnique(int[] num) {
 		ArrayList<ArrayList<Integer>> returnList = new ArrayList<ArrayList<Integer>>();
 		returnList.add(new ArrayList<Integer>());
+		
+		for(int i=0; i<num.length; i++) {
+			Set<ArrayList<Integer>> currentSet=new HashSet<ArrayList<Integer>>();
+		}
 		
 		return returnList;
 	}
