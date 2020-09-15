d69dfc2a5ae2a6bb8ee8eb728dc016b9f58ccec1
yeonsu100 <yeonsu100@gmail.com>
2020-09-15 23:04:17 +0900

    Permutations04 - 셋팅

---
+++ JavaCollection/src/permutations/Permutations04.java
@@ -0,0 +1,12 @@
+package permutations;
+
+import java.util.ArrayList;
+
+public class Permutations04 {
+	public static ArrayList<ArrayList<Integer>> permuteUnique(int[] num) {
+		ArrayList<ArrayList<Integer>> returnList = new ArrayList<ArrayList<Integer>>();
+		returnList.add(new ArrayList<Integer>());
+		
+		return returnList;
+	}
+}
