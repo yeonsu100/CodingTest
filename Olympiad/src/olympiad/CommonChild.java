dcd4e49881f5f1b4b892c77a0b7eaf8fc25decfb
yeonsu100 <yeonsu100@gmail.com>
2020-09-30 00:02:42 +0900

    Common child - valueLength 추가, 리턴값 추가

--- Olympiad/src/olympiad/CommonChild.java
+++ Olympiad/src/olympiad/CommonChild.java
@@ -7,9 +7,14 @@ public class CommonChild {
 		Scanner sc=new Scanner(System.in);
 		String str1=sc.next();
 		String str2=sc.next();
+		
+		int result=commonChild(str1, str2);
 	}
 	
 	public static int commonChild(String str1, String str2) {
+		int[][] valueLength=new int[str1.length()+1][str2.length()+1];
 		
+		int result=valueLength[str1.length()][str2.length()];
+		return result;
 	}
 }
