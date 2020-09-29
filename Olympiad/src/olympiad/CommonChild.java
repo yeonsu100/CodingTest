a3970afa842592b70d5c9bb9a1ff9b6e4a0fc9fe
yeonsu100 <yeonsu100@gmail.com>
2020-09-30 00:16:57 +0900

    Common child solved

--- Olympiad/src/olympiad/CommonChild.java
+++ Olympiad/src/olympiad/CommonChild.java
@@ -9,10 +9,22 @@ public class CommonChild {
 		String str2=sc.next();
 		
 		int result=commonChild(str1, str2);
+		
+		System.out.println(result);
 	}
 	
 	public static int commonChild(String str1, String str2) {
 		int[][] valueLength=new int[str1.length()+1][str2.length()+1];
+		
+		for(int i=0; i<str1.length(); i++) {
+			for(int j=0; j<str2.length(); j++) {
+				if(str1.charAt(i)==str2.charAt(j)) {
+					valueLength[i+1][j+1]=valueLength[i][j]+1;
+				}else {
+					valueLength[i+1][j+1]=Math.max(valueLength[i+1][j], valueLength[i][j+1]);
+				}
+			}
+		}
 		
 		int result=valueLength[str1.length()][str2.length()];
 		return result;
