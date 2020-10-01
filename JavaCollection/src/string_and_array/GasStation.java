73af21f5aa307e163b5dcc9ed1b214bf188cc97c
yeonsu100 <yeonsu100@gmail.com>
2020-10-02 00:15:46 +0900

    Gas Station add some comments

--- JavaCollection/src/string_and_array/GasStation.java
+++ JavaCollection/src/string_and_array/GasStation.java
@@ -2,14 +2,17 @@ package string_and_array;
 
 public class GasStation {
 	public int canCompleteCircuit(int[] gas, int[] cost) {
-		int sumRemaining=0;
-		int total=0;
+		int sumRemaining=0;		// track current remaining
+		int total=0;			// track total remaining
 		int start=0;
 		
 		for(int i=0; i<gas.length; i++) {
 			int remaining=gas[i]-cost[i];
+			
+			// if sum remaining of (i-1) >= 0, continue
 			if (sumRemaining>=0) {
 				sumRemaining += remaining;
+			// otherwise, reset start index to be current
 			}else {
 				sumRemaining=remaining;
 				start=i;
